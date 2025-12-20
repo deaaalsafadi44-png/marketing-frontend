26ed4cd36ec7a825f676fe0dc9c189a3d122514b
deaaalsafadi44-png <YOUR_EMAIL@example.com>
2025-12-21 01:54:34 +0300

    fix routes paths

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,18 @@
 import Navbar from "../components/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "./MainLayout.css";
 
 const MainLayout = () => {
+  const location = useLocation();
+
+  const fullBgPages = ["/users/add"];
+
+  const isFullBg = fullBgPages.includes(location.pathname);
+
   return (
     <div className="layout-container">
       <Navbar />
-      <div className="page-content">
+      <div className={`page-content ${isFullBg ? "full-bg" : ""}`}>
         <Outlet />
       </div>
     </div>
