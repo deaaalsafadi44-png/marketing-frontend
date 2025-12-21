ddb704466410752762996b1e398bfbed4e8259c7
deaaalsafadi44-png <YOUR_EMAIL@example.com>
2025-12-21 03:02:50 +0300

    fix routes paths

--- src/pages/Users/EditUser.jsx
+++ src/pages/Users/EditUser.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getUserById, updateUserApi } from "../../services/usersService";
 import { useNavigate, useParams } from "react-router-dom";
-import "./editUser.css";
+
 
 const EditUser = () => {
   const { id } = useParams(); // Mongo _id
